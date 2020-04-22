a32d5bb26ecdf39a6f7ad21f4b65332bebd6e336
Ablanco1547 <63083462+Ablanco1547@users.noreply.github.com>
2020-04-22 19:20:39 -0400

    cambios en secciones y footer
    cambios en secciones y footer

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+
+
+/*Botones de la seccion joyeria*/
+
+
+$(document).ready(function() {
+    $("#botonFunc1").click(function() {
+      $("#botonToggle1").toggle(200000);
+    });
+  });
+
+  
+
+  $(document).ready(function() {
+    $("#botonFunc2").click(function() {
+      $("#botonToggle2").toggle(200000);
+    });
+  });
+
+  $(document).ready(function() {
+    $("#botonFunc3").click(function() {
+      $("#botonToggle3").toggle(200000);
+    });
+  });
